39f999140bb80307af3d8a444cc371fb764e72ed
Baudev <baudev.contact@gmail.com>
2018-06-16 23:50:22 +0200

    Add Logs class

---
+++ src/helpers/Logs.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: Baudev
+ * Date: 15/06/2018
+ */
+
+namespace FCMStream\helpers;
+
+class Logs
+{
+
+    /**
+     * Verbose logs levels
+     */
+    public CONST DEBUG = 2;
+    public CONST WARN = 1;
+    public CONST ANY = 0;
+
+    /**
+     * Function that write logs
+     * @param int $level Level of the log. It declares if the log must be written depending of the verbose mode of the script.
+     * @param string $title Title
+     * @param string $content Content
+     */
+    public static function writeLog(int $level, string $title, string $content = '')
+    {
+        if (Configuration::getDebugFile() && $level <= Configuration::getDebugLevel()) {
+            echo "=== $title === " . new DatetimeISO8601() . " ===\n" . strlen(trim($content)) ? trim($content) . "\n" : "";
+            fwrite(Configuration::getDebugFile(), "=== $title === " . new DatetimeISO8601() . " ===\n" . (strlen(trim($content)) ? trim($content) . "\n" : ""));
+        }
+    }
+
+}
