d50e683b86315d04e929b615e7ea8e10c93f21b8
Baudev <baudev.contact@gmail.com>
2019-05-07 19:35:26 +0200

    Add test for not fragmented message in Core analyze method

--- src/tests/CoreTest.php
+++ src/tests/CoreTest.php
@@ -23,6 +23,13 @@ class CoreTest extends \PHPUnit\Framework\TestCase
         return $method;
     }
 
+    public function testNotFragmentedMessageBehavior()
+    {
+        $method = self::getMethodOfCore('analyzeData');
+        $method->invokeArgs(self::$obj, array('<message to="00000000000@fcm.googleapis.com" from="devices@gcm.googleapis.com" type="normal"><gcm xmlns="google:mobile:data">{"data":{"message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},"time_to_live":86400,"from":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx","message_id":"000001","category":"com.example.myapplication"}</gcm></message>'));
+        $this->expectOutputRegex('#=== Message is not fragmented. === \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{4} ===\\n#');
+    }
+
     public function testKeepAliveMessageBehavior()
     {
         $method = self::getMethodOfCore('analyzeData');
